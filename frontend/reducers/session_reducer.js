ba10ce711bd4da31084a6bf2d93a88394f0414d9
Grant <grantpaulson6@gmail.com>
2019-05-11 16:29:09 -0700

    left off at the main page part of frontend auth in the walkthrough

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
+
+export default function(state = initialState, action) {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_USER_LOGOUT:
+            return {
+                isAuthenticated: false,
+                user: undefined
+            };
+        default:
+            return state;
+    }
+}
