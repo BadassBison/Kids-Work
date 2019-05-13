ad045ad8705fac63c14daa3e6bee1901e6e9b228
Sean Kennealy <spkennealy@gmail.com>
2019-05-13 14:06:27 -0700

    Add folders for ui, errors, & entities in reducers, add ui_reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
-import errors from './errors_reducer';
+import errors from './errors/errors_reducer';
 
 const RootReducer = combineReducers({
     session,
