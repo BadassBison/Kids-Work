ed584bcd944c51acfabf546d4ea144c20586c12a
BadassBison <shawn.scott.xd@gmail.com>
2019-05-12 21:56:11 -0700

    Push to Heroku

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,7 +3,14 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import Login from './session/parent/login_form_container';
 import Signup from './session/parent/signup_form_container';
+const path = require('path');
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
 
 const App = () => (
     // <Switch>
