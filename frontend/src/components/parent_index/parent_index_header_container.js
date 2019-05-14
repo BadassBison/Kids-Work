8afa15ae94da55325cb6e1d7e725a09065134647
BadassBison <shawn.scott.xd@gmail.com>
2019-05-14 11:27:15 -0700

    Parent index components started and built with placeholders

---
+++ frontend/src/components/parent_index/parent_index_header_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux'
+import ParentIndexheader from './Parent_index_header'
+
+const mapStateToProps = state => {
+    return ({
+       
+    })
+}
+
+const mapDispatchToProps = dispatch => {
+    return ({
+       
+    })
+}
+
+export default connect(mapStateToProps, null)(ParentIndexHeader)
