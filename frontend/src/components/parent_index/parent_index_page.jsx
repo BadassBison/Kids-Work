0ce6fc29eb7a7f8be42191b9a529ea35c7b3f62d
BadassBison <shawn.scott.xd@gmail.com>
2019-05-15 10:06:32 -0700

    Parent tweaks

--- frontend/src/components/parent_index/parent_index_page.jsx
+++ frontend/src/components/parent_index/parent_index_page.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ParentIndexheader from './parent_index_header_container';
-import ParentIndexBody from './parent_index_body';
+// import ParentIndexBody from './parent_index_body';
+import MainLinks from "../page_components/main_links_container";
+import ChildLinks from "./child_links_container";
+import CurrentChores from "../page_components/current_chores_container";
 
 
 export default class ParentIndexPage extends Component {
@@ -13,13 +16,51 @@ export default class ParentIndexPage extends Component {
   render() {
     return (
       <div>
+
+        <section class="section">
+          <div class="container">
+
+          
+          <div class="tile is-ancestor">
+              <div class="tile is-vertical is-12">
                 
-        <section className="section">
-          <div className="container">
-            <ParentIndexheader />
-            <ParentIndexBody />
+                <div class="tile">
+                  
+                  <div class="tile is-parent is-vertical">
+                    <article class="tile is-child notification is-info">
+                      <ParentIndexheader />
+                    </article>
+
+                    <article class="tile is-child notification is-warning">
+                      <MainLinks />
+                      <ChildLinks />
+                      <CurrentChores />
+                    </article>
+                    
+                    <article class="tile is-child notification is-primary">
+                      <p class="title">Dashboard</p>
+                    </article>
+                  </div>
+
+                </div>
+              </div>
+
+              {/* <div class="tile is-parent">
+                <article class="tile is-child notification is-success">
+                  <div class="content">
+                    <p class="title">Available Chores</p>
+                    <h2>Container</h2>
+                  </div>
+                </article>
+              </div>*/}
+            </div> 
           </div>
-        </section> 
+        </section>
+
+
+
+
+
       </div>
     )
   }
