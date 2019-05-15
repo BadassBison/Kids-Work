fe2a3241043170b000fb1343a24f6dc443f6e2d9
Grant <grantpaulson6@gmail.com>
2019-05-14 20:23:05 -0700

    chores are populated and filtering properly

--- frontend/src/components/page_components/current_chores.jsx
+++ frontend/src/components/page_components/current_chores.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
 export default class CurrentChores extends Component {
+
+
+  
   render() {
+    const chores = this.props.chores.map(chore => {
+  
+      return (
+        <div className="box"><span>{chore.title}</span> <span>${chore.amount}</span></div>
+      )
+    })
+
     return (
         <div>
-          <div className="box"><span>Wipe table</span> <span>$1.00</span></div>
+          {/* <div className="box"><span>Wipe table</span> <span>$1.00</span></div>
           <div className="box"><span>Dishes</span> <span>$1.00</span></div>
           <div className="box"><span>Clean Room</span> <span>$1.00</span></div>
           <div className="box"><span>Sweep Floor</span> <span>$1.00</span></div>
-          <div className="box"><span>Walk Dog</span> <span>$1.00</span></div>  
+          <div className="box"><span>Walk Dog</span> <span>$1.00</span></div>   */}
+          {chores}
         </div>  
     )
   }
