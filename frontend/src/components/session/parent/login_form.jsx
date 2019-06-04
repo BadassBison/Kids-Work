5b79eaf8aa668d96c55b995a56497b4e62808187
Sean Kennealy <spkennealy@gmail.com>
2019-06-04 11:47:11 -0700

    Add demo user for parent & child, must click login once typing is completed.. for now

--- frontend/src/components/session/parent/login_form.jsx
+++ frontend/src/components/session/parent/login_form.jsx
@@ -5,6 +5,7 @@ import Title from '../../display_components/title/title'
 import SubTitle from '../../display_components/sub_title/sub_title'
 import SubmitField from '../../input_components/submit_field/submit';
 import Switch from '../../dashboard_components/switch/switch';
+import Typed from 'typed.js';
 
 class LoginForm extends React.Component {
     constructor(props) {
@@ -18,6 +19,8 @@ class LoginForm extends React.Component {
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.toggleSwitch = this.toggleSwitch.bind(this);
+        this.clearInputs = this.clearInputs.bind(this);
+        this.demoUser = this.demoUser.bind(this);
     }
 
     update(field) {
@@ -64,6 +67,97 @@ class LoginForm extends React.Component {
         });
     }
 
+    clearInputs() {
+        document.getElementsByClassName("first-name-input").value = "";
+        document.getElementsByClassName("family-name-input").value = "";
+        document.getElementsByClassName("password-input").value = "";
+    }
+
+    demoUser(e) {
+        e.preventDefault();
+        this.clearInputs();
+
+        if (this.state.isParent) {
+            const typedFirstName = {
+                strings: ["Steph"],
+                typeSpeed: 70
+            };
+    
+            const typedFamilyName = {
+                strings: ["Curry"],
+                typeSpeed: 70
+            };
+    
+            const typedPassword = {
+                strings: ["password"],
+                typeSpeed: 70
+            };
+    
+            this.setState({
+                firstName: "",
+                familyName: "",
+                password: ""
+            });
+    
+            new Typed(".first-name-input", typedFirstName);
+    
+            setTimeout(() => {
+                new Typed(".family-name-input", typedFamilyName);
+            }, 1000);
+    
+            setTimeout(() => {
+                new Typed(".password-input", typedPassword);
+            }, 2000);
+    
+            setTimeout(() => {
+                this.props.loginParent({
+                    firstName: "Steph",
+                    familyName: "Curry",
+                    password: "password"
+                });
+            }, 2500);
+        } else {
+            const typedFirstName = {
+                strings: ["Riley"],
+                typeSpeed: 70
+            };
+
+            const typedFamilyName = {
+                strings: ["Curry"],
+                typeSpeed: 70
+            };
+
+            const typedPassword = {
+                strings: ["password"],
+                typeSpeed: 70
+            };
+
+            this.setState({
+                firstName: "",
+                familyName: "",
+                password: ""
+            });
+
+            new Typed(".first-name-input", typedFirstName);
+
+            setTimeout(() => {
+                new Typed(".family-name-input", typedFamilyName);
+            }, 1000);
+
+            setTimeout(() => {
+                new Typed(".password-input", typedPassword);
+            }, 2000);
+
+            setTimeout(() => {
+                this.props.loginChild({
+                    firstName: "Riley",
+                    familyName: "Curry",
+                    password: "password"
+                });
+            }, 2500);
+        }
+    }
+
     render() {
         return (
             <section className="hero is-primary form-container">
@@ -84,11 +178,18 @@ class LoginForm extends React.Component {
                             <Switch 
                                 loginSwitch={"login-switch"} />
                         </button>
+                    </div> 
+                    <div className="login-switch-container">
+                        <button
+                            type="button"
+                            className="splash-nav-link"
+                            onClick={this.demoUser}
+                        >DEMO USER</button>
                     </div>
                     <form className="login-form" onSubmit={this.handleSubmit}>
-                        <UserField value={this.props.firstName} onChange={this.update("firstName")} placeholder="First Name"/>
-                        <UserField value={this.props.familyName} onChange={this.update("familyName")} placeholder="Family Name"/>
-                        <PasswordField password={this.props.password} onChange={this.update("password")} placeholder="Password"/>
+                        <UserField value={this.props.firstName} addClass="first-name-input" onChange={this.update("firstName")} placeholder="First Name"/>
+                        <UserField value={this.props.familyName} addClass="family-name-input" onChange={this.update("familyName")} placeholder="Family Name"/>
+                        <PasswordField password={this.props.password} addClass="password-input" onChange={this.update("password")} placeholder="Password"/>
                         <SubmitField value="Sign In" />
                     </form>
                 </div>
